7aabf98bd14f3da0e4d3c539757a862c04d9d435
Anikesh02 <anikeshkulalinft-@atharvacoe.ac.in>
2025-01-10 12:02:04 +0530

    Header Component

--- components/header.jsx
+++ components/header.jsx
@@ -1,18 +1,47 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs"
-
+import { SignedIn, SignedOut, SignInButton } from "@clerk/nextjs";
+import { PenBox } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "./ui/button";
+import UserMenu from "./user-menu";
 
 const Header = () => {
   return (
     <div>
-        <SignedOut>
-            <SignInButton />
-        </SignedOut>
+      <header className="container mx-auto ">
+        <nav className="py-6 px-4 flex justify-between items-center">
+          <Link href="/">
+            <Image
+              src={"/logo2.png"}
+              alt="planorama logo"
+              width={200}
+              height={56}
+              className="h-16 w-auto object-contain"
+            />
+          </Link>
 
-        <SignedIn>
-            <UserButton />
-        </SignedIn>
+          <div className="flex items-center gap-4">
+            <Link href="/project/create">
+              <Button variant="destructive" className="flex items-center gap-2">
+                <PenBox size={18} />
+                <span>Create Project</span>
+              </Button>
+            </Link>
+
+            <SignedOut>
+              <SignInButton forceRedirectUrl="/onboarding">
+              <Button variant="outline">Login</Button>
+              </SignInButton>
+            </SignedOut>
+
+            <SignedIn>
+              <UserMenu />
+            </SignedIn>
+          </div>
+        </nav>
+      </header>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
