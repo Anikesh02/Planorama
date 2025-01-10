9a4164a98211a8bf52aa3d9d3daa47acda54f0d1
Anikesh02 <anikeshkulalinft-@atharvacoe.ac.in>
2025-01-10 16:46:55 +0530

    Implemented Landing Page and Postgres Setup

--- components/header.jsx
+++ components/header.jsx
@@ -1,6 +1,5 @@
 import { SignedIn, SignedOut, SignInButton } from "@clerk/nextjs";
 import { PenBox } from "lucide-react";
-import Image from "next/image";
 import Link from "next/link";
 import { Button } from "./ui/button";
 import UserMenu from "./user-menu";
@@ -11,13 +10,14 @@ const Header = () => {
       <header className="container mx-auto ">
         <nav className="py-6 px-4 flex justify-between items-center">
           <Link href="/">
-            <Image
-              src={"/logo2.png"}
+            {/* <Image
+              src={"/logo.png"}
               alt="planorama logo"
               width={200}
               height={56}
               className="h-16 w-auto object-contain"
-            />
+            /> */}
+            <h1 className=" text-5xl font-bold tracking-wide bg-gradient-to-br from-yellow-400 via-yellow-100 to-yellow-600 bg-clip-text text-transparent">Planorama</h1>
           </Link>
 
           <div className="flex items-center gap-4">
